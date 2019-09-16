ee0eb0823698e30557d541c336d064d57403ce15
震彪 谭 <baby0112u@163.com>
2019-09-16 20:58:33 +0800

    'ClientCredential'

---
+++ ThirdPartyClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ThirdPartyClient {
+    class Program {
+        static async Task Main(string[] args) {
+
+            // TokenClient 发现 
+            var tokenClient = new HttpClient();
+            var disco = await tokenClient.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError) {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenResponse = await tokenClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest {
+                Address = disco.TokenEndpoint,  // Address = disco.TokenEndpoint 请求Token的 TokenEndpoint
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError) {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode) {
+                Console.WriteLine(response.StatusCode);
+            } else {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
